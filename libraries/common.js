51ac688e11e7c0acb0b66b161756e2d0e9f52907
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2012-08-05 19:57:50 +0200

    Common javascript file created

---
+++ libraries/common.js
@@ -0,0 +1,14 @@
+function eliminateDuplicates(arr) {
+  var i,
+      len=arr.length,
+      out=[],
+      obj={};
+
+  for (i=0;i<len;i++) {
+    obj[arr[i]]=0;
+  }
+  for (i in obj) {
+    out.push(i);
+  }
+  return out;
+}
