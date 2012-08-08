5946a708dbcbc1525cd56fb7af5eafe19ebdf12f
Dario Rodighiero <fumoseaffabulazioni@gmail.com>
2012-08-08 13:01:15 +0200

    Vocabulary has been revised with cookies and a selection menu

--- libraries/vocabularies.js
+++ libraries/vocabularies.js
@@ -1,11 +1,42 @@
-var lang = window.navigator.userLanguage || window.navigator.language;
+setCookie(document.cookie.split("=")[1]);
 
-setLanguege(lang.substr(0, 2));
+function setCookie(requestedLang)
+{
+	requestedLang = (requestedLang) ? requestedLang.substr(0, 2) : "";
+	currentLang = (document.cookie.split("=")[1]) ? document.cookie.split("=")[1] : "";
 
-function setLanguege(lang) {
+	// console.log("requestedLang " + requestedLang)
+	// console.log("currentLang " + currentLang)
 
-	if ( lang == "en" ) { loadLanguage(lang); }
+	// First use, nothing exists
+
+	if ( requestedLang == "" && currentLang == "" ) {
+		console.log("First use, nothing exists")
+		lang = "en"; // Default
+		document.cookie = "lang=" + lang;
+		loadLanguage(lang);
+	}
+
+	// Second use, requestedLang and currentLang are the same
+
+	if ( requestedLang == currentLang ) {
+		console.log("Second use, requestedLang and currentLang are the same")
+		lang = currentLang; // Reused
+		loadLanguage(lang);
+	}
+
+	// Third case, requestedLang and currentLang are different, user wants to change language
+
+	if ( requestedLang != currentLang ) {
+		console.log("Third case, requestedLang and currentLang are different, user wants to change language")
+		lang = requestedLang;
+		document.cookie = "lang=" + lang;
+		window.location.reload();
+	}
+
+	return false;
 }
+
 
 function loadLanguage(lang) {
 	d3.json("data/vocabularies/" + lang + ".json", function(json) {
@@ -14,6 +45,6 @@ function loadLanguage(lang) {
 			$("." + json.data[key].class)
 				.text(json.data[key].value);
 		}
-
+		
 	});
 }
